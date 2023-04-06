215121ac3f3f7e76c954d4c4c4df07ba4317d74b
farrah fahira <farrahfahira2202@gmail.com>
2023-04-07 05:51:09 +0700

    laman donasi

---
+++ public/js/donasi.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  // Hide all tab content
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.remove("show");
+  }
+
+  // Deactivate all tab buttons
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  // Show the selected tab content and activate the button
+  document.getElementById(tabName).classList.add("show");
+  evt.currentTarget.classList.add("active");
+}
